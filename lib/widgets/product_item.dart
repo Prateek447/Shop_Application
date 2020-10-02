83a9c5304931d8cf009e08153530166c3f6e4a4a
DREAM_DEVELOPER <46447285+developerstrange@users.noreply.github.com>
2020-10-01 22:45:39 -0500

    Initial commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+//  final String description;
+  final String imageUrl;
+
+  ProductItem(
+      {@required this.id, @required this.title, @required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
